2072a7be4413de864b5341be2bba4814630a9b41
sarareibschied <SR251148@muhlenberg.edu>
2016-10-24 21:47:36 -0400

    first commit

---
+++ Tests.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import unittest
+import os
+
+class HomePageTest(unittest.TestCase):
+    
+    def setUp(self):
+        if os.name=='nt':
+            self.browser = webdriver.Chrome()
+        else:
+            self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
